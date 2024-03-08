12be249b8f1aeb3469233e6bf1220e994ef629d8
Álisson <alissonwegner@gmail.com>
2024-03-08 17:16:53 -0300

    teste de log
    testes de diferente tipo de log

--- event_security.py
+++ event_security.py
@@ -1,28 +1,16 @@
-# do a script who reads windows logs and print the last 10 logs
-
-
-
-# Path: main.py
 import win32evtlog
-import win32api
-import win32con
 
 server = 'localhost'
-logtype = 'System'
+logtype = 'Security'  # Especificando o tipo de log como 'Security' para acessar os eventos de segurança
 hand = win32evtlog.OpenEventLog(server, logtype)
 flags = win32evtlog.EVENTLOG_BACKWARDS_READ | win32evtlog.EVENTLOG_SEQUENTIAL_READ
 total = win32evtlog.GetNumberOfEventLogRecords(hand)
 
 events = win32evtlog.ReadEventLog(hand, flags, 0)
-#events = [events]
+
 for event in events:
+    # Aqui você pode processar os eventos de segurança conforme necessário
     print(event.StringInserts)
     print(event.TimeGenerated)
     print(event.EventID)
-    print(event.EventType)
-    print(event.SourceName)
-    print(event.ComputerName)
-    print(event.RecordNumber)
-    print(event.TimeWritten)
-    print(event.TimeGenerated)
-    print(event.EventCategory)
+    # Adicione outras propriedades conforme necessário
